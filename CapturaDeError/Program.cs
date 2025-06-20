07d16a2a86983d124224a74ee4a9cd80f1219171
David Paez <jeffersonpaez5@gmail.com>
2025-06-20 17:36:19 -0400

    Add project files.

---
+++ CapturaDeError/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+//1.0
+
+namespace CapturaDeError
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Escribe Un Numero");
+                int Numero = int.Parse(Console.ReadLine());
+                Console.WriteLine("El numero es : " + Numero);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error de Capa 8, intruducir un Numero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocurrio Un Error" + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Proceso Finalizado");
+            }
+            
+        }
+        
+            
+    }
+}
